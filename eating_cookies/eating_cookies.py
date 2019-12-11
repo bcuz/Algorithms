1d8a66e1f1946eca4db0290adb4c1e390ba25cc1
adam <adam.nawroc@gmail.com>
2019-12-10 23:19:51 -0500

    thinking

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,6 +8,15 @@ import sys
 def eating_cookies(n, cache=None):
   pass
 
+# 4 cookie brainstorm
+# He can eat 1 cookie at a time 4 times
+# He can eat 1 cookie, 1 cookie, then 2 cookies
+# He can eat 1 cookie, 2 cookies, then 1 cookies
+# He can eat 2 cookie, 1 cookies, then 1 cookies
+# He can eat 2 cookies, then 2 cookies
+# He can eat 1 cookies, then 3 cookies
+# He can eat 3 cookies, then 1 cookies
+
 if __name__ == "__main__":
   if len(sys.argv) > 1:
     num_cookies = int(sys.argv[1])
