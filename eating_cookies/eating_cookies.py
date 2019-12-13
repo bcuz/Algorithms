93ad50028809d32149224d9e8014fa38f91beabc
adam <adam.nawroc@gmail.com>
2019-12-12 23:00:19 -0500

    optimize eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -12,10 +12,16 @@ def eating_cookies(n, cache=None):
     return 2
   elif n == 3:
     return 4
+  elif cache and cache[n] > 0:
+    return cache[n]
+  else:
+    if not cache:
+      cache = {i: 0 for i in range(n+1)}
 
-  return eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies( n - 1)
+  cache[n] = eating_cookies(n - 3, cache) + eating_cookies(n - 2, cache) + eating_cookies( n - 1, cache)
+  return cache[n]
 
-print(eating_cookies(0))
+# print(eating_cookies(0))
 
 # 4 cookie brainstorm
 # He can eat 1 cookie at a time 4 times
